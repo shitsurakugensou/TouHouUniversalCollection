3b197264b7b368543eaeb150f5225268b373050c
SaigyoujiYuyuko <purple_guy@qq.com>
2019-04-14 11:46:45 +0800

    更新统计

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -356,8 +356,8 @@
                         <div class="mdui-valign mdui-typo">
                             <p class="copyright" style="padding-left: 4px;">Copyright © 2019 - {{ date("Y",time()) }} <a href="https://github.com/SaigyoujiYuyuko233">SaigyoujiYuyuko[3558168775]</a> . All rights reserved.</p>
 
-                            @if(env("APP_DEBUG") == false)
-                                <script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? "https://" : "http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1277106802'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s5.cnzz.com/z_stat.php%3Fid%3D1277106802%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"));</script>
+                            @if(env("APP_DEBUG") != true)
+                                <script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? "https://" : "http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1277106802'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s5.cnzz.com/z_stat.php%3Fid%3D1277106802%26show%3Dpic1' type='text/javascript'%3E%3C/script%3E"));</script>
                             @endif
                         </div>
                     </div>

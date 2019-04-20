3ccc185f1214e8c255b2c4f126e05fd949a1a9dc
SaigyoujiYuyuko <purple_guy@qq.com>
2019-04-20 16:56:18 +0800

    类名重用
    尝试修复Cannot declare class Annoncements, because the name is already in use

--- database/migrations/2019_04_20_050030_annoncements_table.php
+++ database/migrations/2019_04_20_050030_annoncements_table.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class Annoncements extends Migration
+class AnnoncementsTable extends Migration
 {
     /**
      * Run the migrations.

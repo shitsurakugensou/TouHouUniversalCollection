08bc2853f3f1480dda8da6a1c977b1d66cef3875
SaigyoujiYuyuko <purple_guy@qq.com>
2019-04-20 17:32:19 +0800

    PhpUnit - AccessRoot
    根目录访问测试

--- tests/Feature/Access/RootTest.php
+++ tests/Feature/Access/RootTest.php
@@ -2,21 +2,23 @@
 
 namespace Tests\Feature\Access;
 
+use JakubOnderka\PhpConsoleColor\ConsoleColor;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class RootTest extends TestCase
-{
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function testExample(){
+class RootTest extends TestCase{
+
+    public function testHttpCode(){
+        $color = new ConsoleColor();
+
         $response = $this->get('/');
         $response->assertStatus(200);
 
-        echo "[Testing]";
+        echo $color->apply("color_39","\n[Testing]");
+        echo $color->apply("color_13","[Access/RootTest] ");
+        echo $color->apply("color_40","Successfully to access '/' -- 200 √\n");
     }
+
+
 }

3d043e6b2ac4b798049862e715e44cc8c7f85328
SaigyoujiYuyuko <purple_guy@qq.com>
2019-04-20 19:08:35 +0800

    更新颜色库 | trci.yml
    更换了color-cil
    trci 增加环境
    准备支持codecov.io

--- tests/Feature/Access/RootTest.php
+++ tests/Feature/Access/RootTest.php
@@ -2,23 +2,25 @@
 
 namespace Tests\Feature\Access;
 
-use JakubOnderka\PhpConsoleColor\ConsoleColor;
+use Colors\Color;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class RootTest extends TestCase{
 
+    // check the http code
     public function testHttpCode(){
-        $color = new ConsoleColor();
+
+        $color = new Color();
 
         $response = $this->get('/');
         $response->assertStatus(200);
 
-        echo $color->apply("color_39","\n[Testing]");
-        echo $color->apply("color_13","[Access/RootTest] ");
-        echo $color->apply("color_40","Successfully to access '/' -- 200 √\n");
-    }
+        echo $color->apply("magenta","\n[Testing]");
+        echo $color->apply("light_blue","[Access/RootTest] ");
+        echo $color->apply("light_green","Successfully to access '/' - 200 √\n");
 
+    }
 
 }

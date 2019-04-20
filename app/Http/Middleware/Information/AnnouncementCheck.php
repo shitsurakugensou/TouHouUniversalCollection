c141b651f34333493d64c4bf36d61855cd1cfa55
SaigyoujiYuyuko <purple_guy@qq.com>
2019-04-20 09:26:57 +0800

    fix: 数据库公告=null时查询报错 trci回掉钩子

--- app/Http/Middleware/Information/AnnouncementCheck.php
+++ app/Http/Middleware/Information/AnnouncementCheck.php
@@ -18,8 +18,13 @@ class AnnouncementCheck
      * @return mixed
      */
     public function handle($request, Closure $next){
-        $announcement = DB::table("information")->select("Announcement")->get('Announcement')[0]->Announcement;
-        $announcement = 'swal({title: "公告", html: \'' . $announcement . '\', confirmButtonText: "知道了", type: "info"});';
+        $announcement = DB::table("information")->select("Announcement")->get('Announcement')[0];
+        $announcement = $announcement->Announcement != null ? $announcement->Announcement : null;
+
+        // 如果公告 == null
+        if ($announcement != null){
+            $announcement = 'swal({title: "公告", html: \'' . $announcement . '\', confirmButtonText: "知道了", type: "info"});';
+        }
 
         // 判断是否带有通知cookie | false: 显示通知
         if($announcement != null && Cookie::has("announcement") == false){

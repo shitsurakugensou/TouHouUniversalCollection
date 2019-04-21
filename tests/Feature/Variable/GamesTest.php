1d78fdc4328338d8bbed530cad1a74bf23eb5422
SaigyoujiYuyuko <purple_guy@qq.com>
2019-04-21 08:14:27 +0800

    完善所有变量检测
    finish that

--- tests/Feature/Variable/GamesTest.php
+++ tests/Feature/Variable/GamesTest.php
@@ -10,19 +10,42 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 class GamesTest extends TestCase{
 
 
-    // for the var: $announcement - Array
-    public function testAnnouncementsCheck(){
+    // for the var: $list - Array
+    public function testGamesCheck(){
 
         $color = new Color();
 
-        $response = $this->get('/');
+        $response = $this->get('/view/resources/games');
 
-        $response->assertSee("Test #0")
-            ->assertViewIs('pages.home');
+        $response->assertSee("qwq")
+            ->assertViewIs('pages.games');
 
         echo $color->apply("magenta","\n[Testing]");
-        echo $color->apply("light_blue","[Variable/RootTest] ");
-        echo $color->apply("light_green","Successfully to see var - [announcement] √\n");
+        echo $color->apply("light_blue","[Variable/GamesTest] ");
+        echo $color->apply("light_green","Successfully to see var - [list->gamename] √\n");
+
+
+        $response->assertSee("test game")
+            ->assertViewIs('pages.games');
+
+        echo $color->apply("magenta","\n[Testing]");
+        echo $color->apply("light_blue","[Variable/GamesTest] ");
+        echo $color->apply("light_green","Successfully to see var - [list->description] √\n");
+
+
+        $response->assertSee("0")
+            ->assertViewIs('pages.games');
+
+        echo $color->apply("magenta","\n[Testing]");
+        echo $color->apply("light_blue","[Variable/GamesTest] ");
+        echo $color->apply("light_green","Successfully to see var - [list->download_times] √\n");
+
+        $response->assertSee("/images/touhou.png")
+            ->assertViewIs('pages.games');
+
+        echo $color->apply("magenta","\n[Testing]");
+        echo $color->apply("light_blue","[Variable/GamesTest] ");
+        echo $color->apply("light_green","Successfully to see var - [list->game_logo] √\n");
 
     }
 
---
+++ tests/Feature/Variable/ToolsTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Tests\Feature\Variable;
+
+use Colors\Color;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ToolsTest extends TestCase
+{
+
+    // for the var: $list - Array
+    public function testToolsCheck(){
+
+        $color = new Color();
+
+        $response = $this->get('/view/resources/tools');
+
+        $response->assertSee("qwq")
+            ->assertViewIs('pages.tools');
+
+        echo $color->apply("magenta","\n[Testing]");
+        echo $color->apply("light_blue","[Variable/ToolsTest] ");
+        echo $color->apply("light_green","Successfully to see var - [list->name] √\n");
+
+
+        $response->assertSee("test tools")
+            ->assertViewIs('pages.tools');
+
+        echo $color->apply("magenta","\n[Testing]");
+        echo $color->apply("light_blue","[Variable/ToolsTest] ");
+        echo $color->apply("light_green","Successfully to see var - [list->description] √\n");
+
+
+        $response->assertSee("0")
+            ->assertViewIs('pages.tools');
+
+        echo $color->apply("magenta","\n[Testing]");
+        echo $color->apply("light_blue","[Variable/ToolsTest] ");
+        echo $color->apply("light_green","Successfully to see var - [list->download_times] √\n");
+
+        $response->assertSee("10000")
+            ->assertViewIs('pages.tools');
+
+        echo $color->apply("magenta","\n[Testing]");
+        echo $color->apply("light_blue","[Variable/ToolsTest] ");
+        echo $color->apply("light_green","Successfully to see var - [list->id] √\n");
+
+        $response->assertSee("zip")
+            ->assertViewIs('pages.tools');
+
+        echo $color->apply("magenta","\n[Testing]");
+        echo $color->apply("light_blue","[Variable/ToolsTest] ");
+        echo $color->apply("light_green","Successfully to see var - [list->format] √\n");
+
+    }
+
+}
